e3647a635d8a5c4ec64620b6a6b6a5155740c2df
Abiodun Giwa <biodun1230@gmail.com>
2020-11-30 01:35:20 -0500

    basic drillz test completed

---
+++ src/main/java/com/codedifferently/basic_drills/StringUtilities.java
@@ -0,0 +1,101 @@
+package com.codedifferently.basic_drills;
+
+
+public class StringUtilities {
+    /**
+     * @return `Hello World` as a string
+     */
+    public static String getHelloWorld() {
+        String expected = "Hello World";
+        return expected;
+    }
+
+    /**
+     * @param firstSegment a string to be added to
+     * @param secondSegment a string to add
+     * @return the concatenation of two strings, `firstSegment`, and `secondSegment`
+     */
+    public static String concatenation(String firstSegment, String secondSegment){
+        return firstSegment + secondSegment;
+    }
+
+    /**
+     * @param firstSegment a string to be added to
+     * @param secondSegment a string to add
+     * @return the concatenation of an integer, `firstSegment`, and a String, `secondSegment`
+     */
+    public static String concatenation(int firstSegment, String secondSegment){
+        return firstSegment + secondSegment;
+    }
+
+    /**
+     * @param input a string to be manipulated
+     * @return the first 3 characters of `input`
+     */
+    public static String getPrefix(String input){
+
+        return input.substring(0, 3);
+    }
+
+    /**
+     * @param input a string to be manipulated
+     * @return the last 3 characters of `input`
+     */
+    public static String getSuffix(String input){
+        return input.substring(2,5);
+    }
+
+    /**
+     * @param inputValue the value to be compared
+     * @param comparableValue the value to be compared against
+     * @return the equivalence of two strings, `inputValue` and `comparableValue`
+     */
+    public static Boolean compareTwoStrings(String inputValue, String comparableValue){
+        return inputValue.equalsIgnoreCase(comparableValue);
+    }
+
+    /**
+     * @param inputValue the value input from user
+     * @return the middle character of `inputValue`
+     */
+    public static Character getMiddleCharacter(String inputValue){
+        Integer len = inputValue.length();
+        Integer mid;
+        Character x;
+        if (len %2 == 0) {
+            mid = (len-1)/2;
+        } else {
+            mid = len/2;
+        }
+        x = inputValue.charAt(mid);
+        return x;
+    }
+
+    /**
+     * @param spaceDelimitedString a string, representative of a sentence, containing spaces
+     * @return the first sequence of characters
+     */
+    public static String getFirstWord(String spaceDelimitedString){
+        String arr[] = spaceDelimitedString.split(" ", 2);
+        return arr[0];
+    }
+
+    /**
+     * @param spaceDelimitedString a string delimited by spaces
+     * @return the second word of a string delimited by spaces.
+     */
+    public static String getSecondWord(String spaceDelimitedString){
+        String arr[] = spaceDelimitedString.split(" ", 2);
+        return arr[1];
+    }
+
+    /**
+     * @param stringToReverse
+     * @return an identical string with characters in reverse order.
+     */
+    public static String reverse(String stringToReverse){
+        StringBuilder x = new StringBuilder(stringToReverse);
+        StringBuilder y = x.reverse();
+        return y.toString();
+    }
+}
